768af1dd7e6c51bdd43d135591f35e7546ad1cf0
Your Name <you@example.com>
2024-02-17 23:56:02 +0530

    test

--- lambda_function.py
+++ lambda_function.py
@@ -1,32 +1,7 @@
-import json
-
-def factorial(n):
-    """
-    Calculates the factorial of a positive integer n.
-    """
-    result = 1
-    for i in range(1, n + 1):
-        result *= i
-    return result
+import pandas as pd
 
 def lambda_handler(event, context):
-    try:
-        num = int(event["queryStringParameters"]["num"])
-        if num < 0:
-            response = {
-                "statusCode": 400,
-                "body": json.dumps("Please enter a positive integer.")
-            }
-        else:
-            result = factorial(num)
-            response = {
-                "statusCode": 200,
-                "body": json.dumps({"result": result})
-            }
-    except ValueError:
-        response = {
-            "statusCode": 400,
-            "body": json.dumps("Invalid input. Please enter a valid positive integer.")
-        }
-    
-    return response
+    d = {'col1': [1,2], 'col2': [3,4]}
+    df = pd.DataFrame(data=d)
+    print(df)
+    print('Done x1.1')
