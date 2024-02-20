b674b094c56d58b69cdfdc34c10a6ddfe4bd793d
Your Name <you@example.com>
2024-02-20 12:29:52 +0530

    buildspec file

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,39 @@
-import pandas as pd
+import json
+
+def factorial(n):
+    """
+    Calculates the factorial of a positive integer n.
+    """
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
 
 def lambda_handler(event, context):
-    d = {'col1': [1,2], 'col2': [3,4]}
-    df = pd.DataFrame(data=d)
-    print(df)
-    print('Done x1.1')
+    try:
+        num_str = event["num"]
+        num = int(num_str)
+        
+        if not num_str.isdigit():
+            response = {
+                "statusCode": 400,
+                "body": json.dumps("Please provide a valid integer in the 'num' field.")
+            }
+        elif num < 0:
+            response = {
+                "statusCode": 400,
+                "body": json.dumps("Please provide a positive integer in the 'num' field.")
+            }
+        else:
+            result = factorial(num)
+            response = {
+                "statusCode": 200,
+                "body": json.dumps({"result": result})
+            }
+    except (KeyError, ValueError):
+        response = {
+            "statusCode": 400,
+            "body": json.dumps("Invalid input. Please provide a valid integer in the 'num' field.")
+        }
+    
+    return response
